c0bd2f31f1a19751a6308b80224bec744d82b440
Sanjeev Midda <s.midda1@gmail.com>
2026-03-22 17:42:00 +0000

    created function which squares the input number provided

--- python.py
+++ python.py
@@ -1,3 +1,11 @@
-name = "test"
+# Create variable
+# name = "test"
 
-print(name)
+# print(name)
+
+# create a function which takes an argument
+
+# def squared(number):
+#     return number * number
+
+# print(squared(16))
